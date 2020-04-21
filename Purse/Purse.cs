b640ea08b2558f67386dcb8bc5b0f8c180b59533
Nataliia Dyshko <nataliia.dyshko@ukr.net>
2020-04-21 21:36:38 +0300

    Add instance of BusinessCard to Purse
    Created an instance of BusinessCard class as demonstration of its proper work.

--- Purse/Purse.cs
+++ Purse/Purse.cs
@@ -5,6 +5,10 @@ class Purse
     static public void Main()
     {
         Console.WriteLine("Purse project is running...");
+
+        BusinessCard businessCard = new BusinessCard("1234567890");
+        Console.WriteLine(businessCard.info());
+
         Console.Read();
     }
 }
