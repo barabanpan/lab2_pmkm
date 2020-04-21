f3824cfc875e5789053a37a1f5dfa69dd953f024
Nataliia Dyshko <nataliia.dyshko@ukr.net>
2020-04-21 21:33:54 +0300

    Create BusinessCard.dll

---
+++ BusinessCard/BusinessCard.cs
@@ -0,0 +1,14 @@
+﻿public class BusinessCard
+{
+    string number;
+
+    public BusinessCard(string number)
+    {
+        this.number = number;
+    }
+
+    public string info()
+    {
+        return "BusinessCard witn number " + number;
+    }
+}
