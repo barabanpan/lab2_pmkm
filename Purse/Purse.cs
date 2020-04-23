2fd8928a0edd33a5d42c9011a566f8737d36ba7b
Stanislav Benesh <98thrxse@gmail.com>
2020-04-23 11:52:20 +0300

    Create instance of Money

--- Purse/Purse.cs
+++ Purse/Purse.cs
@@ -5,6 +5,10 @@ class Purse
     static public void Main()
     {
         Console.WriteLine("Purse project is running...");
+
+        Money money = new Money(300);
+        Console.WriteLine(money.HowMuch());
+
         Console.Read();
     }
 }
