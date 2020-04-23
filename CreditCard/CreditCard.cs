5c5770d13724e3c8adc56f84332285fb792e8e8c
Tania Katerynchuk <3598tania@gmail.com>
2020-04-23 12:08:19 +0300

    Create CreditCard.dll

---
+++ CreditCard/CreditCard.cs
@@ -0,0 +1,14 @@
+﻿public class CreditCard
+{
+    string number;
+
+    public CreditCard(string number)
+    {
+        this.number = number;
+    }
+
+    public string info()
+    {
+        return "CreditCard with number " + number;
+    }
+}
