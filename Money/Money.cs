b49d59de1685a321bb283e09ee20e6e93fc7d43f
Stanislav Benesh <98thrxse@gmail.com>
2020-04-23 11:51:52 +0300

    Create Money.dll

---
+++ Money/Money.cs
@@ -0,0 +1,13 @@
+﻿public class Money
+{
+    double amount;
+
+    public Money(double amount)
+    {
+        this.amount = amount;
+    }
+    public string HowMuch()
+    {
+        return "Money:" + amount;
+    }
+}
