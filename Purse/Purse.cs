e24c2e1dac084e27aeacfe506c89819d6e309efb
Tania Katerynchuk <3598tania@gmail.com>
2020-04-23 12:09:38 +0300

    Create instance of CreditCard

--- Purse/Purse.cs
+++ Purse/Purse.cs
@@ -5,6 +5,10 @@ class Purse
     static public void Main()
     {
         Console.WriteLine("Purse project is running...");
+
+        CreditCard creditCard = new CreditCard("0904623442");
+        Console.WriteLine(creditCard.info());
+
         Console.Read();
     }
 }
